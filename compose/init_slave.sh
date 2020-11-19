0644155782978b8b1b5fb6b323f0960093adc1f6
jiezi19971225 <jiezi19971225@gmail.com>
2020-11-19 19:34:50 +0800

    脚本微调

--- compose/init_slave.sh
+++ compose/init_slave.sh
@@ -14,7 +14,10 @@ do
 params[$cnt]=$param
 ((cnt+=1))
 done
+ 
+# 休眠 60s，等待主库启动完成，如果建立主从连接仍然失败，需进入容器手动执行该脚本
 
+sleep 60s
 
 mysql -u root -p${MYSQL_ROOT_PASSWORD} \
 -e "CHANGE MASTER TO MASTER_HOST='db', \
